e62e51f32b02bee2dda8339de6d93b697e765516
Dmytro Misik <d.misik@draftkings.com>
2023-03-06 17:01:08 +0200

    remove region

--- internal/aws/dynamodb.go
+++ internal/aws/dynamodb.go
@@ -2,25 +2,15 @@ package aws
 
 import (
 	"context"
-	"os"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
 func NewDynamoDBClient(ctx context.Context) (*dynamodb.Client, error) {
-	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region()))
+	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		return nil, err
 	}
 	return dynamodb.NewFromConfig(cfg), nil
-}
-
-func region() string {
-	region := os.Getenv("AWS_REGION")
-	if region == "" {
-		region = "eu-central-1"
-	}
-
-	return region
 }
